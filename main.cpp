b4bb84d44e4947ba61d3b2eff5771befcd128a9c
Amey Krishna <113315505+amekrish@users.noreply.github.com>
2023-12-04 15:11:57 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 #include "matrix.h"
 #include "menu.h"
 #include <iostream>
+#include <vector>
 
 int main() {
-  std::cout << "Hello World!\n";
-  std::cout << "This is Nonogram!\n";
+  int rowColCount;
+  std::vector<int> rowHints;
+  std::vector<int> colHints;
+  std::cout << "number of rows/cols?";
+  std::cin >> rowColCount;
+  std::cout << "hints?";
+  for (size_t i = 0; i < rowColCount; ++i) {
+    int x;
+    std::cin >> x;
+    rowHints.push_back(x);
+  }
+  for (size_t j = 0; j < rowColCount; ++j) {
+    int y;
+    std::cin >> y;
+    colHints.push_back(y);
+  }
+  Game *game(rowColCount, rowHints, colHints) = new Game;
+  delete game;
 }
